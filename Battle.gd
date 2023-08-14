d856d1cc14ca4d2de959bbb2eb7cc17bf54cc612
jessica <57881206+fat-beaver@users.noreply.github.com>
2023-08-14 16:51:13 +1200

    Changed settings to allow godot to run from vscode

--- Battle.gd
+++ Battle.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
 func _ready():
-	print("Hello world!")
+	var label = Label.new()
+	label.set_text("Hello world!")
+	add_child(label)
+	var camera = Camera2D.new()
+	add_child(camera)
+	#set the camera as the current one so it is actually used
+	camera.make_current()
 
-func _process(delta):
+func _process(_delta):
 	pass
